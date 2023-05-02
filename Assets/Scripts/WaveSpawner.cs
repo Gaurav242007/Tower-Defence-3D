1f5399fcf4288e6b3ac66f0dd75023b58f086a02
Gaurav242007 <gaurav2499kumar+1@gmail.com>
2023-05-02 15:22:39 +0530

    Added Wave Spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public TMP_Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        // first will take 2second (or default countdown) to spawn a wave
+        // then afterwards will take 5 seconds (or timeBetweenWaves)
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        // reducing countdown to get to  0 and run SpawnWave()
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    // use system.collections while using coroutine
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
