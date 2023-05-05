0129b9b615374097c4213f1374f6f875325dd3e8
Gaurav242007 <gaurav2499kumar+1@gmail.com>
2023-05-05 14:31:32 +0530

    Added Missiles 🚀

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,10 @@ public class Bullet : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
+    // explosion radius for mission launcher 
+    // to explode in a particular area
+    // and damages all enemies within it.
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     public void Seek(Transform _target)
@@ -29,14 +33,54 @@ public class Bullet : MonoBehaviour
             return;
         }
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget()
     {
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         // Destroy effect after 2 seconds
-        Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        Destroy(effectIns, 5f);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+        // destroying the bullet prefab after collision
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        // create a sphere with a radius
+        // and check all the colliders
+        // within the radius
+        // list of colliders (or objects hit)
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            // filtered by tag ==> enemy
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    // viewing the range highlighted
+    // in a 3d sphere grid
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
