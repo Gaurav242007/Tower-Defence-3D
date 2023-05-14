dc6cbf4e254e209f6951de5a9a0ba0b919ad5f81
Gaurav242007 <gaurav2499kumar+1@gmail.com>
2023-05-14 21:15:12 +0530

    Fixed Errors

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -6,15 +6,18 @@ using TMPro;
 public class WaveSpawner : MonoBehaviour
 {
     public static int EnemiesAlive = 0;
+
     public Wave[] waves;
-    public Transform enemyPrefab;
+
     public Transform spawnPoint;
 
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
 
     public TMP_Text waveCountdownText;
+
     public GameManager gameManager;
+
     private int waveIndex = 0;
 
     void Update()
@@ -29,7 +32,6 @@ public class WaveSpawner : MonoBehaviour
             gameManager.WinLevel();
             this.enabled = false;
         }
-        Debug.Log(countdown);
         // first will take 2second (or default countdown) to spawn a wave
         // then afterwards will take 5 seconds (or timeBetweenWaves)
         if (countdown <= 0f)
@@ -40,8 +42,10 @@ public class WaveSpawner : MonoBehaviour
         }
         // reducing countdown to get to  0 and run SpawnWave()
         countdown -= Time.deltaTime;
-        // adjusting/clamping the countdown value between 0 and infinity 
-        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+        if (countdown <= 0f)
+        {
+            countdown = 0f;
+        }
         // formatting to time string
         waveCountdownText.text = string.Format("{0:00.00}", countdown);
     }
